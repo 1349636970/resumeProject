163a180ac03878001802f941043763606affc825
1349636970 <a1349636970@gmail.com>
2018-07-01 12:03:25 -0700

    not finish yet

---
+++ resources/views/resume/common/layout.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="keywords">
+    <meta name="description" content="description">
+    <title>Type your resume</title>
+    <link rel="stylesheet" href="{{ asset("css/bootstrap.css") }}">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+    <script src="{{ asset("js/bootstrap.js") }}"></script>
+    @section("js/css")
+
+    @show
+</head>
+<body>
+@section("content")
+
+@show
+</body>
+</html>
